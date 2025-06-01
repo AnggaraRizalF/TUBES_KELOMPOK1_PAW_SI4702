78b4d2fbaf0322ff9c8d6b31874cb281bb5bfb8b
AnggaraRF <anggaidabkk@gmail.com>
2025-06-01 21:00:03 +0700

    models kegiatan dan user

--- sikema-finals/app/Models/User.php
+++ sikema-finals/app/Models/User.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -17,10 +17,13 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'nim',
+        'role',
     ];
 
     /**
@@ -43,6 +46,34 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role' => 'string',
         ];
     }
+    public function pendaftaranKegiatans()
+    {
+        return $this->hasMany(PendaftaranKegiatan::class);
+    }
+
+    public function taks()
+    {
+        return $this->hasMany(TAK::class);
+    }
+
+    public function sertifikats()
+    {
+        return $this->hasMany(Sertifikat::class);
+    }
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isUser()
+    {
+        return $this->role === 'user';
+    }
+
 }
+
+
